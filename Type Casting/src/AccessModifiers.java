2e2947a6869d9d1966929657c1a152f75943f2ae
d7oomy1997 <asdasd20101@hotmail.com>
2021-06-10 18:03:53 +0300

    Access Modifiers

---
+++ Type Casting/src/AccessModifiers.java
@@ -0,0 +1,15 @@
+
+public class AccessModifiers {
+
+	void tast() {
+		System.out.println("this is how to use defalut access specifier ");
+	}
+}
+ class main{	
+	public static void main(String[] args) {
+		
+		AccessModifiers AM=new AccessModifiers();
+		AM.tast();
+	}
+
+}
